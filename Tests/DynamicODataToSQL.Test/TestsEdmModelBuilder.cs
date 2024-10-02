79c61cb92034e6d3c87d897a319aa22228df9fad
vaibhav-goyal <goyalvaibhav@outlook.com>
2024-10-02 23:13:45 +0530

    upgrade packages and format

--- Tests/DynamicODataToSQL.Test/TestsEdmModelBuilder.cs
+++ Tests/DynamicODataToSQL.Test/TestsEdmModelBuilder.cs
@@ -1,52 +1,49 @@
-﻿namespace DynamicODataToSQL
+namespace DynamicODataToSQL;
+
+using System.Collections.Generic;
+
+using Microsoft.OData.Edm;
+
+/// <inheritdoc/>
+/// <summary>
+/// EdmModelBuilder with known test tables properties.
+/// </summary>
+public class TestsEdmModelBuilder : EdmModelBuilder
 {
-    using System.Collections.Generic;
-    using Microsoft.OData.Edm;
+    private readonly Dictionary<string, Dictionary<string, EdmPrimitiveTypeKind>> _knownTableProperties;
 
-    /// <inheritdoc/>
-    /// <summary>
-    /// EdmModelBuilder with known test tables properties.
-    /// </summary>
-    public class TestsEdmModelBuilder : EdmModelBuilder
+    public TestsEdmModelBuilder() => _knownTableProperties = new Dictionary<string, Dictionary<string, EdmPrimitiveTypeKind>>
     {
-        private readonly Dictionary<string, Dictionary<string, EdmPrimitiveTypeKind>> _knownTableProperties;
-
-        public TestsEdmModelBuilder()
+        ["Products"] = new Dictionary<string, EdmPrimitiveTypeKind>
         {
-            _knownTableProperties = new Dictionary<string, Dictionary<string, EdmPrimitiveTypeKind>>
-            {
-                ["Products"] = new Dictionary<string, EdmPrimitiveTypeKind>
-                {
-                    ["Id"] = EdmPrimitiveTypeKind.Int32,
-                    ["Name"] = EdmPrimitiveTypeKind.String,
-                    ["Type"] = EdmPrimitiveTypeKind.String,
-                    ["TotalInventory"] = EdmPrimitiveTypeKind.Int32,
-                    ["TimeCreated"] = EdmPrimitiveTypeKind.DateTimeOffset,
-                    ["Origin"] = EdmPrimitiveTypeKind.String,
-                    ["Spaced Column"] = EdmPrimitiveTypeKind.String
-                },
-                ["Orders"] = new Dictionary<string, EdmPrimitiveTypeKind>
-                {
-                    ["OrderId"] = EdmPrimitiveTypeKind.Int32,
-                    ["TotalAmount"] = EdmPrimitiveTypeKind.Double,
-                    ["Country"] = EdmPrimitiveTypeKind.String,
-                    ["Amount"] = EdmPrimitiveTypeKind.Double,
-                    ["OrderDate"] = EdmPrimitiveTypeKind.DateTimeOffset,
-                    ["value"] = EdmPrimitiveTypeKind.Double
-                }
-            };
+            ["Id"] = EdmPrimitiveTypeKind.Int32,
+            ["Name"] = EdmPrimitiveTypeKind.String,
+            ["Type"] = EdmPrimitiveTypeKind.String,
+            ["TotalInventory"] = EdmPrimitiveTypeKind.Int32,
+            ["TimeCreated"] = EdmPrimitiveTypeKind.DateTimeOffset,
+            ["Origin"] = EdmPrimitiveTypeKind.String,
+            ["Spaced Column"] = EdmPrimitiveTypeKind.String
+        },
+        ["Orders"] = new Dictionary<string, EdmPrimitiveTypeKind>
+        {
+            ["OrderId"] = EdmPrimitiveTypeKind.Int32,
+            ["TotalAmount"] = EdmPrimitiveTypeKind.Double,
+            ["Country"] = EdmPrimitiveTypeKind.String,
+            ["Amount"] = EdmPrimitiveTypeKind.Double,
+            ["OrderDate"] = EdmPrimitiveTypeKind.DateTimeOffset,
+            ["value"] = EdmPrimitiveTypeKind.Double
         }
+    };
 
-        protected override void AddProperties(EdmEntityType entityType)
+    protected override void AddProperties(EdmEntityType entityType)
+    {
+        var tableName = entityType.Name;
+
+        if (_knownTableProperties.TryGetValue(tableName, out var value))
         {
-            var tableName = entityType.Name;
-
-            if (_knownTableProperties.TryGetValue(tableName, out Dictionary<string, EdmPrimitiveTypeKind>? value))
+            foreach (var column in value)
             {
-                foreach (var column in value)
-                {
-                    entityType.AddStructuralProperty(column.Key, column.Value);
-                }
+                entityType.AddStructuralProperty(column.Key, column.Value);
             }
         }
     }
